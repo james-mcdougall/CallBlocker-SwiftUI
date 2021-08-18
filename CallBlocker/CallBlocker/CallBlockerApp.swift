13da62b4c60a578eb2be0613bbd8bb07f916e65a
james-mcdougall <jameswilliammcdougall@gmail.com>
2021-08-17 19:09:48 -0500

    Initial Project Commit

---
+++ CallBlocker/CallBlocker/CallBlockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallBlockerApp.swift
+//  CallBlocker
+//
+//  Created by James McDougall on 8/17/21.
+//
+
+import SwiftUI
+
+@main
+struct CallBlockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
